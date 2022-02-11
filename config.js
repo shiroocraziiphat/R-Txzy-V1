0f20f312d37fb25750fcbb896ad61cf9b13275f4
shiroocraziiphat <99143773+shiroocraziiphat@users.noreply.github.com>
2022-02-12 05:34:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -41,13 +41,13 @@ global.APIKeys = { // APIKey nya disini
 }
 
 // Sticker WM
-global.packname = 'ZenBotz'
+global.packname = 'ShiroBotz'
 global.author = 'LoppYuuu<3'
 
 global.wait = '「 ⏱️ 」Harap tunggu...'
 global.eror = '「❗」Server ERROR!'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
-global.watermark = '©simp4zen' //change the watermark 
+global.watermark = '©shiro akio' //change the watermark 
 global.image = 'https://telegra.ph/file/1a3eac3701775793618b3.jpg' //change the image
 global.thumbfoto = 'https://telegra.ph/file/0bdd84bd5a8099a789828.jpg'
 
